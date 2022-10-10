766a852f18e329ef0fd477c28c42d32446fb2af6
Xisca04 <xiscacerdavicens@gmail.com>
2022-10-10 10:41:34 +0200

    creacion del script

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    /*
+     4 variables with the player information 
+     */
+
+    public int playerAge = 33; // This variable saves the player's age
+    public float playerSpeed = 3.125f; // This variable saves the player's speed
+    public string playerName = "Xisca"; // This variable saves the player's name
+    public bool isGameOver = false; // This variable saves if the player has lost
+
+}
