ed5c01f0e563b13e25fc5ffaf01325362e00d1cd
Xisca04 <xiscacerdavicens@gmail.com>
2022-11-08 11:41:20 +0100

    repaso_funciones

--- Assets/Scripts/Operations.cs
+++ Assets/Scripts/Operations.cs
@@ -9,15 +9,26 @@ public class Operations : MonoBehaviour
 
     public float num1 = 7f;
     public float num2 = 4f;
-    private void Calculator(float num1 , float num2)
+
+    //functions
+    private void Calculator(float num1, float num2)
     {
         Debug.Log($"Sum: {num1} + {num2} = {num1 + num2}");
         Debug.Log($"Substraction: {num1} - {num2} = {num1 - num2}");
         Debug.Log($"Product: {num1} * {num2} = {num1 * num2}");
         Debug.Log($"Division: {num1} / {num2} = {num1 / num2}");
+
     }
 
-    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Calculator(num1, num2);
+        }
+    }
+
+    //operations
     private void Start()
     {
        
